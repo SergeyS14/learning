6aa35372b2b8b6d4caa9f2582c9a85fd7f5d30a8
SergeS14 <stepashinsergey404@gmail.com>
2024-12-23 16:21:02 +0300

    установил и настроил eslint и stylelint

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div :class="$style.title">ler</div>
+</template>
+
+<style lang="scss" module>
+  .title {
+    font-size: 200px;
+  }
+</style>
