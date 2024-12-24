a4b5df6ec224a76e8638d56b54ec783c7a2c4fdf
SergeS14 <stepashinsergey404@gmail.com>
2024-12-24 13:23:07 +0300

    изменил правила работы eslint и stylelint

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
+const message = ref('hello')
 
+const handleClick = () => console.log('hello')
 </script>
 
 <template>
-    <div :class="$style.title">ler</div>
+  <div>
+    <h1 :class="$style.title">
+      ler
+    </h1>
+    <button @click="handleClick">
+      click
+    </button>
+  </div>
 </template>
 
 <style lang="scss" module>
-  .title {
-    font-size: 200px;
+$primary-color: #fff;
+
+.button {
+  color: black;
+  background-color: $primary-color;
+  padding: 10px;
+  margin: 0;
+
+  &:hover {
+    background-color: #f00;
   }
+}
 </style>
